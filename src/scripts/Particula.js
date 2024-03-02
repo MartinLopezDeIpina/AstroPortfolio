f6f8062524868ac1ecf0458b617ef4b075102645
MartinLopezDeIpina <mlopezdeipina001@ikasle.ehu.eus>
2024-03-02 13:50:40 +0100

    sparkles sin acabar

---
+++ src/scripts/Particula.js
@@ -0,0 +1,107 @@
+
+export class Particle {
+    constructor(canvas){
+        this.canvas = canvas;
+        this.circleRadius = canvas.width / 2 ;
+        this.limiteY = this.canvas.height -100;
+        this.disapearing = false;
+        this.x = this.getRandomX();
+        this.y = this.getRandomY();
+        this.size = this.getRandomSize();
+        this.speedX = this.getRandomSpeed();
+        this.speedY = this.getRandomSpeed();
+        this.opacity = Math.random();
+        this.opacityChange = this.getRandomOpacityChange();
+        this.opacityChangePositive = Math.random() > 0.5;
+    }
+
+    move(){
+        this.changeOpacity();
+
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        if(this.particulaFueraDelCirculo() || this.particulaFueraDelLimiteY()){
+            this.disapearing = true;
+        }
+
+        if(this.particulaFueraDelCanvas()){
+            this.respawnSparkle();
+        }
+    }
+
+    particulaFueraDelCanvas(){
+        return this.x > this.canvas.width || this.y > this.canvas.height || this.x < 0 || this.y < 0
+    }
+
+    particulaFueraDelCirculo(){
+        //Distancia respecto al centro del círculo, el centro de X es canvas.width / 2
+        const distanceX = this.x - this.canvas.width / 2;
+        //Distancia respecto al centro que será 0 pq el el medio circulo está mirando pa abajo
+        const distanceY = this.y;
+        //Teorema de pitágoras pa sacar la distancia
+        const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+        //Si la distancia es mayor que el radio del círculo (width/2), la particula está fuera
+        return distance > this.circleRadius;
+    }
+
+    particulaFueraDelLimiteY(){
+        return this.y > this.limiteY;
+    }
+
+    respawnSparkle(){
+        this.x = this.getRandomX();
+        this.y = this.getRandomY();
+        this.disapearing = false;
+    }
+
+    getRandomX(){
+        // Genera una distancia aleatoria dentro del radio del círculo
+        const radius = (Math.random()-0.5) * this.circleRadius;
+        // Calcula la coordenada x dentro del círculo
+        return this.canvas.width / 2 + radius;
+    }
+    getRandomY(){
+        // Genera una distancia aleatoria dentro del límite en el eje Y
+        return Math.random() * this.limiteY;
+    }
+    getRandomSpeed(){
+        const min = 0.1;
+        const max = 1;
+        const random = min + (Math.random() * (max - min));
+        return random > 0.5 ? random : -random;
+    }
+    getRandomSize(){
+        return Math.random();
+    }
+
+
+    getRandomOpacityChange(){
+        return Math.random() * 0.05;
+    }
+    changeOpacity(){
+        if(this.disapearing){
+            if(this.opacity > 0){
+                this.opacity -= this.opacityChange;
+            }else{
+                this.respawnSparkle();
+            }
+            return;
+        }
+        if(this.opacityChangePositive){
+            this.opacity += this.opacityChange;
+            this.changeOpacityDirection();
+        }else{
+            this.opacity -= this.opacityChange;
+            this.changeOpacityDirection();
+        }
+    }
+    changeOpacityDirection(){
+        if(this.opacity > 1){
+            this.opacityChangePositive = false;
+        }
+        if(this.opacity < 0){
+            this.opacityChangePositive = true;
+        }
+    }
+}
---
+++ src/scripts/sparkles.js
@@ -0,0 +1,32 @@
+import {Particle} from './Particula.js';
+
+let canvas = document.getElementById('lienzo');
+
+canvas.addEventListener('canvasResized', () => {
+    let ctx = canvas.getContext('2d');
+
+
+    console.log("ejecutando script");
+
+    const particulas = [];
+
+    for (let i = 0; i < 500; i++) {
+        particulas.push(new Particle(canvas));
+    }
+
+    function dibujarParticulas(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (let i = 0; i < particulas.length; i++) {
+            ctx.fillStyle = 'rgba(255, 255, 255, ' + particulas[i].opacity + ')';
+            ctx.beginPath();
+            ctx.arc(particulas[i].x, particulas[i].y, particulas[i].size, 0, Math.PI * 2);
+            ctx.closePath();
+            ctx.fill();
+        }
+    }
+
+    setInterval(() => {
+        particulas.forEach(p => p.move());
+        dibujarParticulas();
+    }, 30);
+});
